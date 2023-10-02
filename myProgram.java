6863f85fda41c313ead61aa6337f4ab938f3b425
HonngJia <honngjia@gmail.com>
2023-10-02 13:18:35 -0400

    This is my first commit

--- myProgram.java
+++ myProgram.java
@@ -2,4 +2,8 @@ class myProgram{
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public int addNumbers(int a, int b){
+        return a + b;   
+    }
 }
